036549b95e7199559db18086ebd4a07f0e5881a0
Shahzaibjee786 <pc19574.shahzaib@gmail.com>
2023-12-19 14:35:15 +0500

    App completed with functionalities i.e using by Random()

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -1,3 +1,5 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 
 class HomePage extends StatefulWidget {
@@ -8,6 +10,29 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  TextEditingController _textColntroller1 = TextEditingController();
+  TextEditingController _textColntroller2 = TextEditingController();
+
+  bool widgetVisible = false;
+  String text = '';
+
+  void showWidget() {
+    setState(() {
+      widgetVisible = true;
+      Random random = Random();
+      int randomNumber = random.nextInt(40) + 60;
+      text = randomNumber.toString();
+    });
+  }
+
+  void hideWidget() {
+    setState(() {
+      widgetVisible = false;
+      _textColntroller1.clear();
+      _textColntroller2.clear();
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -27,99 +52,109 @@ class _HomePageState extends State<HomePage> {
             ),
             Padding(
               padding: EdgeInsets.all(25),
-              child: Column(
-                children: [
-                  SizedBox(
-                    height: 35,
-                  ),
-                  TextField(
-                    decoration: InputDecoration(
-                      enabledBorder: OutlineInputBorder(
-                          borderSide: BorderSide(
-                              color: Colors.red.shade900, width: 2.0),
-                          borderRadius: BorderRadius.circular(50.0)),
-                      focusedBorder: OutlineInputBorder(
-                          borderSide: BorderSide(
-                              color: Colors.amber.shade900, width: 2.0),
-                          borderRadius: BorderRadius.circular(50.0)),
-                      hintText: "Enter You Name",
+              child: SingleChildScrollView(
+                child: Column(
+                  children: [
+                    SizedBox(
+                      height: 35,
                     ),
-                  ),
-                  SizedBox(
-                    height: 10,
-                  ),
-                  Icon(
-                    Icons.favorite,
-                    size: 60,
-                    color: Colors.red.shade900,
-                  ),
-                  SizedBox(
-                    height: 10,
-                  ),
-                  TextField(
-                    decoration: InputDecoration(
-                      enabledBorder: OutlineInputBorder(
-                          borderSide: BorderSide(
-                              color: Colors.red.shade900, width: 2.0),
-                          borderRadius: BorderRadius.circular(50.0)),
-                      focusedBorder: OutlineInputBorder(
-                          borderSide: BorderSide(
-                              color: Colors.amber.shade900, width: 2.0),
-                          borderRadius: BorderRadius.circular(50.0)),
-                      hintText: "Enter Your Partner Name",
+                    TextField(
+                      style: TextStyle(color: Colors.white),
+                      controller: _textColntroller1,
+                      decoration: InputDecoration(
+                        enabledBorder: OutlineInputBorder(
+                            borderSide: BorderSide(
+                                color: Colors.red.shade900, width: 2.0),
+                            borderRadius: BorderRadius.circular(50.0)),
+                        focusedBorder: OutlineInputBorder(
+                            borderSide: BorderSide(
+                                color: Colors.amber.shade900, width: 2.0),
+                            borderRadius: BorderRadius.circular(50.0)),
+                        hintText: "Enter You Name",
+                      ),
                     ),
-                  ),
-                  SizedBox(
-                    height: 20,
-                  ),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                    children: [
-                      TextButton(
-                          style: TextButton.styleFrom(
-                            shape: RoundedRectangleBorder(
-                              borderRadius: BorderRadius.circular(15.0),
+                    SizedBox(
+                      height: 10,
+                    ),
+                    Icon(
+                      Icons.favorite,
+                      size: 60,
+                      color: Colors.red.shade900,
+                    ),
+                    SizedBox(
+                      height: 10,
+                    ),
+                    TextField(
+                      style: TextStyle(color: Colors.white),
+                      controller: _textColntroller2,
+                      decoration: InputDecoration(
+                        enabledBorder: OutlineInputBorder(
+                            borderSide: BorderSide(
+                                color: Colors.red.shade900, width: 2.0),
+                            borderRadius: BorderRadius.circular(50.0)),
+                        focusedBorder: OutlineInputBorder(
+                            borderSide: BorderSide(
+                                color: Colors.amber.shade900, width: 2.0),
+                            borderRadius: BorderRadius.circular(50.0)),
+                        hintText: "Enter Your Partner Name",
+                      ),
+                    ),
+                    SizedBox(
+                      height: 20,
+                    ),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                      children: [
+                        TextButton(
+                            style: TextButton.styleFrom(
+                              shape: RoundedRectangleBorder(
+                                borderRadius: BorderRadius.circular(15.0),
+                              ),
+                              foregroundColor: Colors.white,
+                              backgroundColor: Colors.pink.shade400,
+                              padding: const EdgeInsets.all(16.0),
+                              textStyle: const TextStyle(fontSize: 17),
                             ),
-                            foregroundColor: Colors.white,
-                            backgroundColor: Colors.pink.shade400,
-                            padding: const EdgeInsets.all(16.0),
-                            textStyle: const TextStyle(fontSize: 17),
-                          ),
-                          onPressed: () {},
-                          child: Text('Calculate')),
-                      TextButton(
-                          style: TextButton.styleFrom(
-                            shape: RoundedRectangleBorder(
-                              borderRadius: BorderRadius.circular(15.0),
+                            onPressed: showWidget,
+                            child: Text('Calculate')),
+                        TextButton(
+                            style: TextButton.styleFrom(
+                              shape: RoundedRectangleBorder(
+                                borderRadius: BorderRadius.circular(15.0),
+                              ),
+                              foregroundColor: Colors.white,
+                              backgroundColor: Colors.pink.shade400,
+                              padding: const EdgeInsets.all(16.0),
+                              textStyle: const TextStyle(fontSize: 17),
                             ),
-                            foregroundColor: Colors.white,
-                            backgroundColor: Colors.pink.shade400,
-                            padding: const EdgeInsets.all(16.0),
-                            textStyle: const TextStyle(fontSize: 17),
-                          ),
-                          onPressed: () {},
-                          child: Text('Try Again'))
-                    ],
-                  ),
-                  Visibility(
-                      child: Stack(
-                    children: [
-                      Positioned(
-                          child: Icon(
-                        Icons.favorite,
-                        size: 255,
-                        color: Colors.red.shade900,
-                      )),
-                      Positioned(
-                          top: 90,
-                          left: 70,
-                          child: Text(
-                            '100%',
-                            style: TextStyle(fontSize: 50, color: Colors.white),
-                          ))
-                    ],
-                  ))
-                ],
+                            onPressed: hideWidget,
+                            child: Text('Try Again'))
+                      ],
+                    ),
+                    Visibility(
+                        visible: widgetVisible,
+                        child: Stack(
+                          children: [
+                            Positioned(
+                                child: Icon(
+                              Icons.favorite,
+                              size: 255,
+                              color: Colors.red.shade900,
+                            )),
+                            Positioned(
+                                top: 90,
+                                left: 80,
+                                child: Text(
+                                  '$text%',
+                                  style: TextStyle(
+                                      fontSize: 50,
+                                      color: Colors.white,
+                                      fontWeight: FontWeight.bold),
+                                ))
+                          ],
+                        ))
+                  ],
+                ),
               ),
             )
           ],
